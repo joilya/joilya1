438e448c3e6f4531594d56cd3561642ea348303a
softjs4 <softjs4@DESKTOP-J89AF69>
2023-07-10 10:57:40 +0900

    This is Commit Message!

---
+++ src/com/joilya/numberstring/String.java
@@ -0,0 +1,10 @@
+package com.joilya.numberstring;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("생활코딩");
+	}
+
+}
---
+++ src/com/joilya/var/Interger.java
@@ -0,0 +1,12 @@
+package com.joilya.var;
+
+public class Interger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a = 1;
+		System.out.println(a+1);
+	}
+
+}
